49d65aefc5bc640483e89dcaead30ddfec9d5689
choldener <choldener@users.noreply.github.com>
2020-08-23 01:07:11 -0500

    Update reformat_functions.py
    Added CIP upload function
    removed CIP standalone export function/buttons
    included CIP export in program_data_variables
    started adding associate functions
    started adding overall_major_functions
    renamed major to 'Academic Program Name'
    removed bonus extremes from masters and doctorate
    functions return their dataframes in preperation for full worksheet exportation

--- reformat_functions.py
+++ reformat_functions.py
@@ -7,6 +7,10 @@ from tkinter.filedialog import askopenfilename
 rawdata = pd.read_csv(
     r"D:\Github\Projects\Belmont-OCPD\Data\2019\csv\Class of 2019 First Destination Survey Raw Data no identifiers scrubbed version 3.3.csv",
     encoding="ISO-8859-1")
+global cip
+cip = pd.read_csv(
+    r"D:\Github\Projects\Belmont-OCPD\Data\2019\CIP codes\Argos Report.csv", encoding="ISO-8859-1"
+)
 
 
 def load_data_handshake():  # Loads the handshake data
@@ -21,8 +25,6 @@ def load_cip_data():  # Loads the CIP data (typically from argos report)
     filename = askopenfilename()
     global cip
     cip = pd.read_csv(filename, encoding="ISO-8859-1")
-    canvas1.create_window(100, 150, window=grad_cip_button)
-    canvas1.create_window(100, 200, window=under_cip_button)
     canvas1.create_window(350, 250, window=program_data_undergrad_button)
     canvas1.create_window(350, 200, window=program_data_masters_button)
     canvas1.create_window(350, 150, window=program_data_doctorate_button)
@@ -32,7 +34,12 @@ def load_cip_data():  # Loads the CIP data (typically from argos report)
     cip_button.destroy()
 
 
-def program_data_variables(major, education, rawdata):
+def program_data_variables(major, education, rawdata, cip):
+    try:
+        program_data_variables.CIP_code = cip.loc[cip.MajorDesc == major, 'CIPCode'].values[0]
+    except:
+        program_data_variables.CIP_code = np.nan
+
     program_data_variables.total_graduated = rawdata[(rawdata['Recipient Primary Major'] == major) &
                                                      (rawdata['Recipient Education Level'] == education)].shape[0]
 
@@ -292,12 +299,13 @@ def overall_data_variables(education, rawdata):
     return ()
 
 
-def bachelors_program_data_function():
-    education = 'Bachelors'
+def associate_program_data_function():
+    education = 'Associate'
     education_level = rawdata[rawdata["Recipient Education Level"].str.startswith(education, na=False)]
     majors = education_level['Recipient Primary Major'].unique()
     majors = pd.DataFrame(majors, columns=['major'])
-    df = pd.DataFrame(columns=['major',
+    df = pd.DataFrame(columns=['Academic Program Name',
+                               'CIP Code',
                                'Total Graduated',
                                'Full-Time',
                                'Part-Time',
@@ -327,9 +335,11 @@ def bachelors_program_data_function():
                                'Bonus Low',
                                'Bonus High'])
     for i in majors['major']:
-        program_data_variables(i, education, rawdata=rawdata)
+        program_data_variables(i, education, rawdata=rawdata, cip=cip)
+        print(cip)
         data_list = []
         df_list = [i,
+                   program_data_variables.CIP_code,
                    program_data_variables.total_graduated,
                    program_data_variables.full_time,
                    program_data_variables.part_time,
@@ -361,6 +371,190 @@ def bachelors_program_data_function():
                    ]
         for var in df_list:
             data_list.append(var)
+        s = pd.Series(data_list, index=df.columns)
+        df = df.append(s, ignore_index=True)
+        print(df)
+    filename = asksaveasfile(defaultextension=".csv",
+                             filetypes=(("Comma-separated values file", "*.csv"),
+                                        ("All Files", "*.*")))
+    df.to_csv(filename, line_terminator='\n')
+    return df
+
+
+def associate_overall_data_function():
+    return df
+
+
+def bachelors_program_data_function():
+    education = 'Bachelors'
+    education_level = rawdata[rawdata["Recipient Education Level"].str.startswith(education, na=False)]
+    majors = education_level['Recipient Primary Major'].unique()
+    majors = pd.DataFrame(majors, columns=['major'])
+    df = pd.DataFrame(columns=['Academic Program Name',
+                               'CIP Code',
+                               'Total Graduated',
+                               'Full-Time',
+                               'Part-Time',
+                               'Entrepreneur Full-Time',
+                               'Entrepreneur Part-Time',
+                               'Temp/Contract FT',
+                               'Temp/Contract PT',
+                               'Freelance  FT',
+                               'Freelance PT',
+                               'Fellowship/Intern FT',
+                               'Fellowship/Intern PT',
+                               'service',
+                               'Military',
+                               'Continuing Education',
+                               'Seeking Employment',
+                               'Seeking Education',
+                               'Not Seek',
+                               'no info',
+                               '# of Salaries (Full-time Employed)',
+                               'Salary Mean',
+                               'Median Salary',
+                               'Salary Low',
+                               'Salary High',
+                               'Bonus Numbers',
+                               'Bonus Mean',
+                               'Bonus Median',
+                               'Bonus Low',
+                               'Bonus High'])
+    for i in majors['major']:
+        program_data_variables(i, education, rawdata=rawdata, cip=cip)
+        print(cip)
+        data_list = []
+        df_list = [i,
+                   program_data_variables.CIP_code,
+                   program_data_variables.total_graduated,
+                   program_data_variables.full_time,
+                   program_data_variables.part_time,
+                   program_data_variables.entrepreneur_ft,
+                   program_data_variables.entrepreneur_pt,
+                   program_data_variables.temp_contract_ft,
+                   program_data_variables.temp_contract_pt,
+                   program_data_variables.freelance_ft,
+                   program_data_variables.freelance_pt,
+                   program_data_variables.fellowship_intern_ft,
+                   program_data_variables.fellowship_intern_pt,
+                   program_data_variables.outcome_service,
+                   program_data_variables.outcome_military,
+                   program_data_variables.outcome_continue_education,
+                   program_data_variables.outcome_still_looking,
+                   program_data_variables.outcome_seeking_education,
+                   program_data_variables.outcome_not_seeking,
+                   program_data_variables.no_info,
+                   program_data_variables.salaries_ft,
+                   program_data_variables.salaries_mean,
+                   program_data_variables.salaries_median,
+                   program_data_variables.salaries_low,
+                   program_data_variables.salaries_high,
+                   program_data_variables.bonus,
+                   program_data_variables.bonus_mean,
+                   program_data_variables.bonus_median,
+                   program_data_variables.bonus_low,
+                   program_data_variables.bonus_high
+                   ]
+        for var in df_list:
+            data_list.append(var)
+        s = pd.Series(data_list, index=df.columns)
+        df = df.append(s, ignore_index=True)
+    filename = asksaveasfile(defaultextension=".csv",
+                             filetypes=(("Comma-separated values file", "*.csv"),
+                                        ("All Files", "*.*")))
+    df.to_csv(filename, line_terminator='\n')
+    return df
+
+
+def bachelors_overall_data_function():
+    df = pd.DataFrame(columns=['Total Graduated', 'Full-Time', 'Part-Time', 'Entrepreneur Full-Time',
+                               'Entrepreneur Part-Time', 'Temp/Contract FT', 'Temp/Contract PT', 'Freelance  FT', 'Freelance PT',
+                               'Fellowship/Intern FT', 'Fellowship/Intern PT',
+                               'Service', 'Military', 'Continuing Education', 'Seeking Employment', 'Seeking Education',
+                               'Not Seek', 'no info', '# of Salaries (Full-time Employed)', 'Salary Mean', 'Median Salary',
+                               'Recieving Bonus', 'Bonus Mean', 'Bonus Median'])
+    data_list = []
+    education = 'Bachelors'
+    df_list = [overall_data_variables.total_graduated,
+               overall_data_variables.ft,
+               overall_data_variables.pt]
+    for i in df_list:
+        data_list.append(i)
+
+    return df
+
+
+def masters_program_data_function():
+    education = 'Masters'
+    education_level = rawdata[rawdata["Recipient Education Level"].str.startswith(education, na=False)]
+    majors = education_level['Recipient Primary Major'].unique()
+    majors = pd.DataFrame(majors, columns=['major'])
+    df = pd.DataFrame(columns=['Academic Program Name',
+                               'CIP Code',
+                               'Total Graduated',
+                               'Full-Time',
+                               'Part-Time',
+                               'Faculty Tenure Track',
+                               'Faculty Non-Tenure Track',
+                               'Entrepreneur Full-Time',
+                               'Entrepreneur Part-Time',
+                               'Temp/Contract FT',
+                               'Temp/Contract PT',
+                               'Freelance  FT',
+                               'Freelance PT',
+                               'Fellowship/Intern FT',
+                               'Fellowship/Intern PT',
+                               'service',
+                               'Military',
+                               'Continuing Education',
+                               'Seeking Employment',
+                               'Seeking Education',
+                               'Not Seek',
+                               'no info',
+                               '# of Salaries (Full-time Employed)',
+                               'Salary Mean',
+                               'Median Salary',
+                               'Salary Low',
+                               'Salary High',
+                               'Bonus Numbers',
+                               'Bonus Mean',
+                               'Bonus Median',
+                               ])
+    for i in majors['major']:
+        program_data_variables(i, education, rawdata=rawdata)
+        data_list = []
+        df_list = [i,
+                   program_data_variables.total_graduated,
+                   program_data_variables.full_time,
+                   program_data_variables.part_time,
+                   program_data_variables.faulty_tenure,
+                   program_data_variables.faculty_nontenure,
+                   program_data_variables.entrepreneur_ft,
+                   program_data_variables.entrepreneur_pt,
+                   program_data_variables.temp_contract_ft,
+                   program_data_variables.temp_contract_pt,
+                   program_data_variables.freelance_ft,
+                   program_data_variables.freelance_pt,
+                   program_data_variables.fellowship_intern_ft,
+                   program_data_variables.fellowship_intern_pt,
+                   program_data_variables.outcome_service,
+                   program_data_variables.outcome_military,
+                   program_data_variables.outcome_continue_education,
+                   program_data_variables.outcome_still_looking,
+                   program_data_variables.outcome_seeking_education,
+                   program_data_variables.outcome_not_seeking,
+                   program_data_variables.no_info,
+                   program_data_variables.salaries_ft,
+                   program_data_variables.salaries_mean,
+                   program_data_variables.salaries_median,
+                   program_data_variables.salaries_low,
+                   program_data_variables.salaries_high,
+                   program_data_variables.bonus,
+                   program_data_variables.bonus_mean,
+                   program_data_variables.bonus_median,
+                   ]
+        for var in df_list:
+            data_list.append(var)
             # print(var)
             # print (data_list)
         # print(data_list)
@@ -372,18 +566,20 @@ def bachelors_program_data_function():
                              filetypes=(("Comma-separated values file", "*.csv"),
                                         ("All Files", "*.*")))
     df.to_csv(filename, line_terminator='\n')
+    return df
 
 
-def bachelors_overall_data_function():
-    return ()
+def masters_overall_data_function():
+    return df
 
 
-def masters_program_data_function():
-    education = 'Masters'
+def doctorate_program_data_function():
+    education = 'Doctorate'
     education_level = rawdata[rawdata["Recipient Education Level"].str.startswith(education, na=False)]
     majors = education_level['Recipient Primary Major'].unique()
     majors = pd.DataFrame(majors, columns=['major'])
-    df = pd.DataFrame(columns=['major',
+    df = pd.DataFrame(columns=['Academic Program Name',
+                               'CIP Code',
                                'Total Graduated',
                                'Full-Time',
                                'Part-Time',
@@ -419,6 +615,7 @@ def masters_program_data_function():
         program_data_variables(i, education, rawdata=rawdata)
         data_list = []
         df_list = [i,
+                   program_data_variables.CIP_code,
                    program_data_variables.total_graduated,
                    program_data_variables.full_time,
                    program_data_variables.part_time,
@@ -463,100 +660,11 @@ def masters_program_data_function():
                              filetypes=(("Comma-separated values file", "*.csv"),
                                         ("All Files", "*.*")))
     df.to_csv(filename, line_terminator='\n')
-
-
-def masters_overall_data_function():
-    return ()
-
-
-def doctorate_program_data_function():
-    education = 'Doctorate'
-    education_level = rawdata[rawdata["Recipient Education Level"].str.startswith(education, na=False)]
-    majors = education_level['Recipient Primary Major'].unique()
-    majors = pd.DataFrame(majors, columns=['major'])
-    df = pd.DataFrame(columns=['major',
-                               'Total Graduated',
-                               'Full-Time', 'Part-Time',
-                               'Faculty Tenure Track',
-                               'Faculty Non-Tenure Track',
-                               'Entrepreneur Full-Time',
-                               'Entrepreneur Part-Time',
-                               'Temp/Contract FT',
-                               'Temp/Contract PT',
-                               'Freelance  FT',
-                               'Freelance PT',
-                               'Fellowship/Intern FT',
-                               'Fellowship/Intern PT',
-                               'service',
-                               'Military',
-                               'Continuing Education',
-                               'Seeking Employment',
-                               'Seeking Education',
-                               'Not Seek',
-                               'no info',
-                               '# of Salaries (Full-time Employed)',
-                               'Salary Mean',
-                               'Median Salary',
-                               'Salary Low',
-                               'Salary High',
-                               'Bonus Numbers',
-                               'Bonus Mean',
-                               'Bonus Median',
-                               'Bonus Low',
-                               'Bonus High'
-                               ])
-    for i in majors['major']:
-        program_data_variables(i, education, rawdata=rawdata)
-        data_list = []
-        df_list = [i,
-                   program_data_variables.total_graduated,
-                   program_data_variables.full_time,
-                   program_data_variables.part_time,
-                   program_data_variables.faulty_tenure,
-                   program_data_variables.faculty_nontenure,
-                   program_data_variables.entrepreneur_ft,
-                   program_data_variables.entrepreneur_pt,
-                   program_data_variables.temp_contract_ft,
-                   program_data_variables.temp_contract_pt,
-                   program_data_variables.freelance_ft,
-                   program_data_variables.freelance_pt,
-                   program_data_variables.fellowship_intern_ft,
-                   program_data_variables.fellowship_intern_pt,
-                   program_data_variables.outcome_service,
-                   program_data_variables.outcome_military,
-                   program_data_variables.outcome_continue_education,
-                   program_data_variables.outcome_still_looking,
-                   program_data_variables.outcome_seeking_education,
-                   program_data_variables.outcome_not_seeking,
-                   program_data_variables.no_info,
-                   program_data_variables.salaries_ft,
-                   program_data_variables.salaries_mean,
-                   program_data_variables.salaries_median,
-                   program_data_variables.salaries_low,
-                   program_data_variables.salaries_high,
-                   program_data_variables.bonus,
-                   program_data_variables.bonus_mean,
-                   program_data_variables.bonus_median,
-                   program_data_variables.bonus_low,
-                   program_data_variables.bonus_high
-                   ]
-        for var in df_list:
-            data_list.append(var)
-            # print(var)
-            # print (data_list)
-        # print(data_list)
-        s = pd.Series(data_list, index=df.columns)
-        # print(s)
-        df = df.append(s, ignore_index=True)
-        # print(df)
-    filename = asksaveasfile(defaultextension=".csv",
-                             filetypes=(("Comma-separated values file", "*.csv"),
-                                        ("All Files", "*.*")))
-    df.to_csv(filename, line_terminator='\n')
+    return df
 
 
 def doctorate_overall_data_function():
-    return ()
+    return df
 
 
 bachelors_program_data_function()
@@ -568,8 +676,6 @@ button_import = tk.Button(root, text='Select .CSV Handshake Data', command=load_
 canvas1.create_window(350, 250, window=button_import)
 
 cip_button = tk.Button(root, text='Select .CSV CIP Data (argos report)', command=load_cip_data)
-grad_cip_button = tk.Button(root, text='Save Graduate CIP Codes', command=graduate_CIP_codes)
-under_cip_button = tk.Button(root, text='Save undergraduate CIP Codes', command=under_CIP_codes)
 
 program_data_undergrad_button = tk.Button(root, text='Save Undergraduate program data', command=bachelors_program_data_function)
 program_data_masters_button = tk.Button(root, text='Save Masters program data', command=masters_program_data_function)
