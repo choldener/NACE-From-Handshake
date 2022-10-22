56eb30846d43bc36d00cc246c3fba89d63dd3d70
Caleb Holdener <55452594+choldener@users.noreply.github.com>
2022-10-22 03:06:27 -0500

    renamed var and added warning

--- tests/test.py
+++ tests/test.py
@@ -12,51 +12,53 @@ def main():
     global compiled
     data = rawdata
     compiled = rawdata
+    raw_size = rawdata.shape[0]
 
     f = open('C:/Users/User/Documents/GitHub/NACE-From-Handshake/test.json')
     json_data = json.load(f)
     #print(json_data)
     for tab_name in json_data:
         data = rawdata
-        conditional = False
+        py_function_bool = False
         shape = True
         for tab_key in json_data[tab_name]:
-            if (tab_key == 'Conditional'):
-                conditional = True
-                conditional_string = json_data[tab_name]['Conditional']
-                shape = checkShape(conditional_string)
+            if (tab_key == 'Py Function'):
+                py_function_bool = True
+                py_function_string = json_data[tab_name]['Py Function']
+                shape = checkShape(py_function_string)
                 continue
             
-            if(conditional == True & shape == True):
+            if(py_function_bool == True & shape == True):
                 if(json_data[tab_name][tab_key] == ""):
-                    statement = "data = data[("+ conditional_string +"(data['"+tab_key+"']))]"
+                    statement = "data = data[("+ py_function_string +"(data['"+tab_key+"']))]"
                     exec(statement, globals(), globals())
                     continue
                 else:
-                    statement = "data = data[("+ conditional_string +"(data[json_data[tab_name] == json_data[tab_name][tab_key]))]"
+                    statement = "data = data[("+ py_function_string +"(data[json_data[tab_name] == json_data[tab_name][tab_key]))]"
                     exec(statement, globals(), globals())
                     continue
-            if((conditional == True) & (shape == False)):
+            if((py_function_bool == True) & (shape == False)):
                 if(json_data[tab_name][tab_key] == ""):
-                    statement = "data = "+ conditional_string +"(compiled['"+tab_key+"'])"
+                    statement = "data = "+ py_function_string +"(compiled['"+tab_key+"'])"
                     exec(statement, globals(), globals())
                     continue
                 else:
                     compiled = compiled[compiled[tab_key] == json_data[tab_name][tab_key]]
                     continue
             data = data[data[tab_key] == json_data[tab_name][tab_key]]
-        name = tab_name.replace(' ', '_').replace('-', '_').replace('/', '_')
+        name = tab_name.replace(' ', '_').replace('-', '_').replace('/', '_').replace('#', 'num').replace('(', '').replace(')', '')
         if (shape == False):
             exec(name + " = data")
         else:
+            if(raw_size == data.shape[0]):
+                print("original data matches transformed data, error likely")
             exec(name + " = data.shape[0]")
-        print(name + ": "+ str(locals()[name]))
+        print(tab_name + ": "+ str(locals()[name]))
     f.close() #Close Json File
 
-def checkShape(conditional):
-    #print(conditional)
+def checkShape(py_function):
     shapelist = ["pd.isna"]
-    if (conditional in shapelist):
+    if (py_function in shapelist):
         return True
     else: 
         return False
